226d50ea4e48297f95e865f4d3f7c60e401b5736
hienao <shiwentao666@gmail.com>
2025-07-22 12:00:55 +0800

    docs: 将项目许可证从MIT更改为GPL-3.0并添加版权声明
    更新README.md中的许可证信息，添加GPL-3.0许可证文件
    在所有主要代码文件中添加GPL-3.0版权声明

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -1,3 +1,21 @@
+<!--
+  OpenList STRM - Stream Management System
+  Copyright (C) 2024 OpenList STRM Project
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
 <template>
   <nav class="bg-white shadow-sm border-b">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
